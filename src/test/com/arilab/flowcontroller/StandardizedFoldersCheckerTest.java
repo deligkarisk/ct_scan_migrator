8ce9e6b68d7ead6d37202cf1bcdc765b1dcba82c
deligkarisk <deligkarisk@gmail.com>
2021-10-27 13:20:52 +0900

    tests for StandardizedFoldersChecker

--- src/test/com/arilab/flowcontroller/StandardizedFoldersCheckerTest.java
+++ src/test/com/arilab/flowcontroller/StandardizedFoldersCheckerTest.java
@@ -1,14 +1,116 @@
 package com.arilab.flowcontroller;
 
+import com.arilab.domain.CtScan;
+import com.arilab.system.SystemExit;
+import com.arilab.utils.Config;
+import com.arilab.utils.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.exceptions.verification.NoInteractionsWanted;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class StandardizedFoldersCheckerTest {
 
-    @Test
-    void check() {
+    @Mock
+    Config config;
 
-        // TODO: complete
+    @Mock
+    SystemExit systemExit;
+
+    @Mock
+    FileUtils fileUtils;
+
+    @InjectMocks
+    StandardizedFoldersChecker standardizedFoldersChecker;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void checkIfAllAvailable() {
+
+        // given
+        CtScan ctScan1 = new CtScan();
+        ctScan1.setNewFolderPathAvailable(true);
+        ctScan1.setNewFolderPathAvailableIntheDatabase(true);
+
+        CtScan ctScan2 = new CtScan();
+        ctScan2.setNewFolderPathAvailable(true);
+        ctScan2.setNewFolderPathAvailableIntheDatabase(true);
+
+        List<CtScan> ctScans = new ArrayList<>();
+        ctScans.add(ctScan1);
+        ctScans.add(ctScan2);
+
+        // when
+        standardizedFoldersChecker.check(ctScans);
+
+        // then
+        verifyNoInteractions(systemExit);
+
+    }
+
+    @Test
+    void checkPathFoldersNotAvailable() {
+
+        // given
+
+        when(config.getFailedOutputFile()).thenReturn("/failedoutputfile.csv");
+
+        CtScan ctScan1 = new CtScan();
+        ctScan1.setNewFolderPathAvailable(true);
+        ctScan1.setNewFolderPathAvailableIntheDatabase(true);
+
+        CtScan ctScan2 = new CtScan();
+        ctScan2.setNewFolderPathAvailable(false);
+        ctScan2.setNewFolderPathAvailableIntheDatabase(true);
+
+        List<CtScan> ctScans = new ArrayList<>();
+        ctScans.add(ctScan1);
+        ctScans.add(ctScan2);
+
+        // when
+        standardizedFoldersChecker.check(ctScans);
+
+        // then
+        verify(systemExit).exit(1);
+    }
+
+
+    @Test
+    void checkPathInDatabaseNotAvailable() {
+
+        when(config.getFailedOutputFile()).thenReturn("/failedoutputfile.csv");
+
+        // given
+        CtScan ctScan1 = new CtScan();
+        ctScan1.setNewFolderPathAvailable(true);
+        ctScan1.setNewFolderPathAvailableIntheDatabase(true);
+
+        CtScan ctScan2 = new CtScan();
+        ctScan2.setNewFolderPathAvailable(true);
+        ctScan2.setNewFolderPathAvailableIntheDatabase(false);
+
+        List<CtScan> ctScans = new ArrayList<>();
+        ctScans.add(ctScan1);
+        ctScans.add(ctScan2);
+
+        // when
+        standardizedFoldersChecker.check(ctScans);
+
+        // then
+        verify(systemExit).exit(1);
     }
 }
